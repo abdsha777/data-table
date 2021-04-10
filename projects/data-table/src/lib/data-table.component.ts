7aaf43c3ae0ecfc329bffb61322ba541da046ae9
Marzook_EV <marzook@fantacode.com>
2021-04-10 18:17:43 +0530

    intial commit

---
+++ projects/data-table/src/lib/data-table.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-data-table',
+  template: `
+    <p>
+      data-table works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DataTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/data-table/src/lib/data-table.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { DataTableComponent } from './data-table.component';
+
+
+
+@NgModule({
+  declarations: [
+    DataTableComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    DataTableComponent
+  ]
+})
+export class DataTableModule { }
